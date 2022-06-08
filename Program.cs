f455ef195d205dce065a6bed4196975b570ace81
David-LivingStone <davidajayidavid@gmail.com>
2022-06-08 11:29:48 +0100

    initial message

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace Decision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Work dec = new Work();
+            //dec.GiveAway();
+
+
+
+            //for loop
+            //Work x = new Work();
+            //x.dav();
+            //Console.ReadLine();
+
+
+
+            //Work rev = new Work();
+            //int x= rev.factorial();
+            //Console.WriteLine(x);
+
+            string[] options = new[] { "P", "S", "R" };
+
+            Console.WriteLine("PRESS 'P' KEY: TO SELECT A PAPER");
+            Console.WriteLine("PRESS 'S' KEY: TO SELECT A SCISSORS");
+            Console.WriteLine("PRESS 'R' KEY: TO SELECT A ROCK");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("ENTER AN OPTION FOR THE GAME:");
+            string res = Console.ReadLine().ToUpper();
+            string opt = "";
+            
+            foreach (var item in options)
+            {
+                if (res == item)
+                {
+                    
+                    opt = item;
+                }
+                //Console.WriteLine("Unknown entry");
+
+
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            if (!string.IsNullOrEmpty(opt))
+            {
+
+                Random rnd = new Random();
+                int index = rnd.Next(options.Length);
+                var X = (options[index]);
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("PLAYER ONE: " + res);
+                Console.WriteLine($"PLAYER TWO: {options[index]}");
+                Console.WriteLine("");
+
+                if (res == "R" && X == "S")
+                {
+                    Console.WriteLine("YOU WON!");
+                }
+                else if (res == "S" && X == "P")
+                {
+                    Console.WriteLine("YOU WON!");
+                }
+
+                else if (res == "P" && X == "R")
+                {
+                    Console.WriteLine("YOU WON!");
+                }
+                else if (res == X)
+                {
+                    Console.WriteLine("IT IS A TIE, PLAY AGAIN!");
+                }
+                else
+                {
+                    Console.WriteLine("YOU LOSE!");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Unknown entry, try again");
+
+
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Work.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Decision
+{
+    public class Work
+    {
+        public void Month()
+        {
+            string message = "";
+        Console.WriteLine("ENTER NUMBER FROM 1- 5");
+         var month = int.Parse(Console.ReadLine());
+         switch (month)
+         {
+             case 1:
+                 message = "JANUARY";
+                 break;
+             case 2:
+                 message = "FEBRUARY";
+                 break;
+             default:
+                 message = "INVALID ENTRY";
+                 break;
+
+         }
+            Console.WriteLine(message);
+         }
+        public void GiveAway()
+        {
+            string message = "";
+            Console.WriteLine("LIVINGSTONE GIVEAWAY");
+            Console.WriteLine("Choose a Raffle between 1 - 5:");
+            string res = Console.ReadLine();
+            message = (res == "1") ? "CAR" : "STRAND OF LINT ";
+
+            //Console.Write("YOU WON A {0}.", message);
+            Console.WriteLine("You Entered: {0}, therefore you won a {1}.", res, message);
+        }
+
+        public int HammingDistance(string str1, string str2)
+        {
+            return str1.Where((t, x) => t != str2[x]).Count();
+        }
+        public int factorial()
+        {
+            Console.WriteLine("ENTER A NUMBER");
+            var x = int.Parse(Console.ReadLine());
+
+            int a = 1;
+            int tot = 0;
+            for (int i = 1; i <= x; i++)
+            {
+
+                tot = a *= i;
+
+            }
+            return tot;
+        }
+        public void dav()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("FOUND SEVEN");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
